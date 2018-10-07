488871fa504c19cba37650529d7715a3e0292dc5
Максим Столбухин <maxstbn@gmail.com>
2018-10-07 14:31:36 +0300

    слайдеры

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  ScrollToTopOnMount,
+  SectionsContainer,
+  Section,
+  Header,
+  Footer,
+} from 'react-fullpage';
 import './App.css';
 
 class App extends Component {
   render() {
+    let options = {
+      sectionClassName:     'section',
+      anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
+      scrollBar:            false,
+      navigation:           false,
+      verticalAlign:        false,
+      arrowNavigation:      true
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+          <SectionsContainer className="container" {...options}>
+            <Section color="#69D2E7">Page 1</Section>
+            <Section color="#A7DBD8">Page 2</Section>
+            <Section color="#E0E4CC">Page 3</Section>
+          </SectionsContainer>
+        </div>
     );
   }
 }
