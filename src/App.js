58b23953d43f02e6e1128c48cc2744a2665360a3
Максим Столбухин <maxstbn@gmail.com>
2018-10-07 15:47:53 +0300

    первый слайд

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
-import {
-  ScrollToTopOnMount,
-  SectionsContainer,
-  Section,
-  Header,
-  Footer,
-} from 'react-fullpage';
-import './App.css';
+import 'fullpage.js/vendors/scrolloverflow';
+import fullpage from 'fullpage.js';
+import 'fullpage.js/dist/fullpage.css';
+
+import Page1 from './categories/Page1';
+
+import './css/white.css';
+import './css/main.css';
 
 class App extends Component {
-  render() {
-    let options = {
-      sectionClassName:     'section',
-      anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
-      scrollBar:            false,
-      navigation:           false,
-      verticalAlign:        false,
-      arrowNavigation:      true
-    };
+  componentDidMount() {
+    new fullpage('#fullpage', {
+      verticalCentered: true,
+      scrollOverflow: true,
+      licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    });
 
+    /* eslint-disable */
+    fullpage_api.setAllowScrolling(true);
+  }
+
+  render() {
     return (
-        <div>
-          <SectionsContainer className="container" {...options}>
-            <Section color="#69D2E7">Page 1</Section>
-            <Section color="#A7DBD8">Page 2</Section>
-            <Section color="#E0E4CC">Page 3</Section>
-          </SectionsContainer>
+      <div className="App" id="fullpage">
+        <Page1 />
+        <div className="section" id="section1">
+          <div className="intro">white.css
+            <h1>Section 2</h1>
+          </div>
         </div>
+        <div className="section" id="section2">
+          <div className="intro">
+            <h1>Section 3</h1>
+            <p>Scroll down to see auto-height sections</p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/categories/Page1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../img/logo.png';
+
+const Page1 = () => (
+  <section className="section text-center">
+    <h4>
+      Главное управление архитектуры
+      и градостроительства Московской Области
+    </h4>
+    <img src={logo} />
+    <h3>
+      Концепция стратегии транспортного развития Московской области 2019–2023 г.г.
+    </h3>
+  </section>
+);
+
+export default Page1;
